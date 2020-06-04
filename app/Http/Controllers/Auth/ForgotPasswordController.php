cc68282856fcb8167ef45384a1d34b731383125b
Carlos Clayton <carlos.clayton@gmail.com>
2020-06-04 15:33:55 -0300

    Refactoring ForgotController anottations

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -32,6 +32,27 @@ class ForgotPasswordController extends Controller
         $this->repository = $repository;
     }
 
+    /**
+     * @OA\Post(
+     *      tags={"Autentication"},
+     *      path="/api/password",
+     *      summary="Forgot password",
+     *      description="Receive an email to change password",
+     *      @OA\Parameter(
+     *          name="email",
+     *          description="Email",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       )
+     * )
+     */
     public function password(PasswordUpdateRequest $request)
     {
         $data['password'] = bcrypt($request->get('password'));
@@ -48,6 +69,28 @@ class ForgotPasswordController extends Controller
         }
     }
 
+
+    /**
+     * @OA\Post(
+     *      tags={"Autentication"},
+     *      path="/api/forgot",
+     *      summary="Forgot password",
+     *      description="Receive an email to change password",
+     *      @OA\Parameter(
+     *          name="email",
+     *          description="Email",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       )
+     * )
+     */
     public function sendResetLinkEmail(Request $request)
     {
         $this->validateEmail($request);
